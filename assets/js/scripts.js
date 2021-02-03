88dbd1a0eba4981a84bbf718f7f23868b244443e
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-02-02 23:02:56 -0300

    codigo javascript ejercicios #79814, #79815, #79816

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,7 +18,7 @@ document.getElementById("obtenerNombreEdad").addEventListener('click', () => {
 
 
 
-// EJERCICIO N798112
+// EJERCICIO N79812
 function imprimirArray() {
 	let nombres = new Array("Pablo", "Juan");
 	let apellidos = new Array("Oviedo", "Santiago");
@@ -34,7 +34,8 @@ function imprimirArray() {
 
 
 
-// EJERCICIO N798113
+
+// EJERCICIO N79813
 let promedioNotas = document.getElementById("btn-promedio-notas");
 
 promedioNotas.addEventListener('click', function() {
@@ -48,5 +49,102 @@ promedioNotas.addEventListener('click', function() {
 	suma = notaUno+notaDos+notaTres;
 	prom = (suma)/3;
 
-	document.getElementById("msgThree").innerHTML = `Su promedio es ${prom.toFixed(2)}`;
+	document.getElementById("msgThree").innerHTML = `Su promedio es <b>${prom.toFixed(2)}</b>`;
 });
+
+
+
+
+// EJERCICIO N79814
+function ShowText() {
+	let cadena = prompt("Escriba un objeto: \n- Notebook\n- Celular\n- Perfume\n- Mueble");
+	// cadena = parseInt(cadena);
+
+	switch(cadena) {
+		case "notebook":
+		case "NOTEBOOK":
+		case "Notebook":
+			document.getElementById("msgFour").innerHTML = `Su eleccion fue <b>${cadena}</b>`;
+			break;
+		case "celular":
+		case "CELULAR":
+		case "Celular":
+			document.getElementById("msgFour").innerHTML = `Su eleccion fue <b>${cadena}</b>`;
+			break;
+		case "perfume":
+		case "PERFUME":
+		case "Perfume":
+			document.getElementById("msgFour").innerHTML = `Su eleccion fue <b>${cadena}</b>`;
+			break;
+		case "mueble":
+		case "MUEBLE":
+		case "Mueble":
+			document.getElementById("msgFour").innerHTML = `Su eleccion fue <b>${cadena}</b>`;
+			break;
+		default:
+			document.getElementById("msgFour").innerHTML = "No encontraron resultados."
+	}
+
+	/**
+	 * Note: Condicional switch{case}, solo se puede utilizar ya sea para evaluar cadena de
+	 * textos o numeros previamente convertido de String a Number
+	 */
+}
+
+
+
+
+// EJERCICIO N79815
+function NumberDigitos() {
+	let numero = parseInt(prompt("Favor ingresar numeros del 1 al 99"));
+	
+	if (numero < 0) {
+		document.getElementById("msgCinco").innerHTML = "EL numero debe ser positivo o mayor a 0";
+	} else {
+		if (numero >=1 && numero <=9) {
+			document.getElementById("msgCinco").innerHTML = "EL numero ingresado tiene solo <b>1 DIGITO</b>";
+		} else {
+			if (numero >=10 && numero <=99) {
+				document.getElementById("msgCinco").innerHTML = "EL numero ingresado tiene solo <b>2 DIGITO</b>";
+			} else {
+				document.getElementById("msgCinco").innerHTML = "EL numero ingresado es mayor a 100";	
+			}
+		}
+	}
+
+	/**
+	 * Note: ¡Que pasaria si al ingresar tres digitos, el primero digito sea un 0, el programa lo
+	 * lo cuenta como un digito adicional.
+	 */
+}
+
+
+
+
+// EJERCICIO N79816
+function NumeroMayor() {
+	let num1, num2, num3 = 0;
+
+	num1 = prompt('Ingrese primer numero',' ');
+	num1 = parseInt(num1);
+	num2 = prompt('Ingrese segundo numero',' ');
+	num2 = parseInt(num2);
+	num3 = prompt('Ingrese tercer numero',' ');
+	num3 = parseInt(num3);
+
+	document.getElementById("listNumber").innerHTML = `Numeros ingresados <ul>
+		<li>Primer numero: <b>${num1}</b></li>
+		<li>Segundo numero: <b>${num2}</b></li>
+		<li>Tercer numero: <b>${num3}</b></li>
+	</ul>`;
+
+	if (num1 > num2 && num1 > num3) {
+		document.getElementById("msgSix").innerHTML = `El numero mayor es <u>${num1}</u>`;
+	} else if (num2 > num1 && num2 > num3) {
+		document.getElementById("msgSix").innerHTML = `El numero mayor es <u>${num2}</u>`;
+	} else if (num3 > num1 && num3 > num2) {
+		document.getElementById("msgSix").innerHTML = `El numero mayor es <u>${num3}</u>`;
+	}
+}
+
+
